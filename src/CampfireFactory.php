95edb96733a5fff5abd93a77674421ecdbbe6e60
Graham Campbell <graham@mineuk.com>
2015-01-18 19:14:28 +0000

    Initial commit

---
+++ src/CampfireFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of NotifyMe.
+ *
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NotifyMeHQ\Campfire;
+
+use NotifyMeHQ\NotifyMe\FactoryInterface;
+
+class CampfireFactory implements FactoryInterface
+{
+    /**
+     * Create a new campfire gateway instance.
+     *
+     * @param string[] $config
+     *
+     * @return \NotifyMeHQ\Campfire\CampfireGateway
+     */
+    public function make(array $config)
+    {
+        return new CampfireGateway($config);
+    }
+}
